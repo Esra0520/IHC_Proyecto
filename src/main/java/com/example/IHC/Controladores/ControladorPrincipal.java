f2e666415ad3f01654b8b61e3fed3b9cb1d3219d
FR4NZR <143857584+FR4NZR@users.noreply.github.com>
2023-11-10 00:38:29 -0500

    Update ControladorPrincipal.java

--- src/main/java/com/example/IHC/Controladores/ControladorPrincipal.java
+++ src/main/java/com/example/IHC/Controladores/ControladorPrincipal.java
@@ -18,6 +18,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/princ/")
